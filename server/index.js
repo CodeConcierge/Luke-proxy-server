36576348e931eb5ae6666198d0fac5cfd37f8e31
Lukas Singer <lukassinger@Lukass-MacBook-Pro.local>
2019-02-20 18:42:04 -0800

    Add all components besides top pics

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+})
+
+app.use(express.static(path.join(__dirname + '/../public')));
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
